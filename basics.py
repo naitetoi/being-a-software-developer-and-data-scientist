bb941903a14475f9f8e3466e9a76634387fef16a
naitetoi <145226025+naitetoi@users.noreply.github.com>
2025-04-06 20:39:20 +0000

    basics.py

---
+++ basics.py
@@ -0,0 +1,12 @@
+
+print ("Third year students semester one grades\n")
+print("Student's details\n")
+student_Name=input("Enter your full names ")
+study_Year=("Enter year of study ")
+Semester=input("Enter semester of study ")
+curriculum_Units= input("Enter units taken this semester ")
+print("Student name\n",+student_Name)
+print("Year of study\n",+study_Year)
+print("Semester\n",+Semester)
+print("Units\n",+curriculum_Units)
+ 
